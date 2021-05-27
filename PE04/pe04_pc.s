06e91b62f79d1030a006c44f1a4d8beea331e319
PCassiday88 <patcassiday@gmail.com>
2021-05-26 21:46:17 -0700

    Able to take input and load disks for one peg

--- PE04/pe04_pc.s
+++ PE04/pe04_pc.s
@@ -13,7 +13,7 @@
 
 
         .data
-msg1:   .asciiz "How many disks would you like to play with? "
+msg1:   .asciiz "Choose a number between 1-8 "
 peg:    .asciiz "||\n"
 disk:   .asciiz "="
 base:   .asciiz "X"
@@ -24,20 +24,17 @@ endl:   .asciiz "\n"
 
 
 showAllDisks:
-        # Display multiple pegs from largest to smallest
-        addi    $sp, $sp, -8                    # Saving s0 and ra
-        sw      $s0, 4($sp)
-        sw      $ra, 0($sp)
-        ble     $a0, $zero, exit
-        move    $s0, $a0
+# Display multiple pegs from largest to smallest
+#        addi    $sp, $sp, -8                    # Saving t9 and ra
+#        sw      $t9, 4($sp)
+#        sw      $ra, 0($sp)
+#        ble     $a0, $zero, exit
+#        move    $t9, $a0        # $t9 has the int given as param
         li      $t0, 1          # Has the number of markers in disk                        
-        move    $t1, $a0        # Has the number of disks left to add
+        move    $t1, $t9        # Has the number of disks left to add
         li      $t2, 1          # Used to increment markers before moving to next disk
-        # move    $t3, $a0        # $t3 will be used to help show full play area, inlcuding whitespace
-        # li      $t4, 3
-        # mult    $t3, $t4
-        # li      $t4, 1 
         j       showDisk
+
 
 showDisk:
         beq     $t0, $zero, whiteSpace 
@@ -59,8 +56,8 @@ nextDisk:
         j       showDisk
 
 whiteSpace:
-        beq     $s0, $t2, nextPeg
-        sub     $t3, $s0, $t2
+        beq     $t9, $t2, nextPeg
+        sub     $t3, $t9, $t2
         j       innerWhiteSpace
 
     innerWhiteSpace:
@@ -90,21 +87,52 @@ nextPeg:
         
 
 restoreStack:
-        lw      $s0, 4($sp)
+        lw      $t9, 4($sp)
         lw      $ra, 0($sp)
         addi    $sp, $sp, 8
         jr      $ra 
 
 
 main:
+        # Load $s0-$s7 with the possible disk ints
+        li      $s0, 1
+        li      $s1, 2
+        li      $s2, 3
+        li      $s3, 4
+        li      $s4, 5
+        li      $s5, 6
+        li      $s6, 7
+        li      $s7, 8
+
         li      $v0, 4
         la      $a0, msg1
         syscall
         # Read the input
         li      $v0, 5
         syscall
+
         move    $a0, $v0
-        jal     showAllDisks
+        jal     checkInput
+        
+        # move    $a0, $t9 
+        # jal     showAllDisks
+
+checkInput: # This method will make sure the input was an int between 1 and 8
+        addi    $sp, $sp, -8                    # Saving t9 and ra
+        sw      $t9, 4($sp)
+        sw      $ra, 0($sp)
+        ble     $a0, $zero, main
+        bgt     $a0, $s7, main
+        move    $t9, $a0
+        j       showAllDisks
+
+
+
+        # li      $t4, 3
+        # mult    $t4, $a0
+        # mflo    $t4  
+        # addi    $t4, 4
+        
 
 exit:
         li      $v0, 10
